2c5dcb469f782c5a4c509c47ddcc7a83bf7fc69b
muhammed zaheer <muhdzaheermv@gmail.com>
2024-09-17 00:55:33 +0530

    configure admin

--- chargespot/settings.py
+++ chargespot/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -137,3 +138,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    'site_header' : "Chargespot",
+    'site_brand':'Book your charge,stay on track',
+    'site_logo':'img/logo.png',
+    'copyright': "chargespot.com",
+
+}
